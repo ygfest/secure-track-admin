07e798b26d49f3d6e7b978be4dc1e7014697884c
Stefano <sanesteban.stefano@gmail.com>
2024-09-09 00:01:34 +0800

    Fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes";
 import "./App.css";
 import { AuthContextProvider } from "./context/AuthContext";
