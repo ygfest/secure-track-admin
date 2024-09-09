cc9ea588646586599274bc516e117988c4739579
Stefano <sanesteban.stefano@gmail.com>
2024-09-09 11:24:44 +0800

    Added a vercel config for server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes";
 import "./App.css";
 import { AuthContextProvider } from "./context/AuthContext";
