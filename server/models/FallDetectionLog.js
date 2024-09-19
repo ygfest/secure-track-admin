2c6b282e5a293b11e4e8aa679bcbdca15f6197d2
ygfest <stefanosanesteban1018@gmail.com>
2024-09-19 17:33:06 +0800

    Changed the model

--- server/models/FallDetectionLog.js
+++ server/models/FallDetectionLog.js
@@ -5,10 +5,10 @@ const ObjectId = Schema.Types.ObjectId;
 const FallDetectionLogSchema = new Schema({
     luggage_tag_number: { type: String, ref: 'Luggage', required: true },
     fall_time: { type: Date, default: Date.now },
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true }
+    message: { type: String, required: true },
+    severity: { type: String, required: true }
 }, {
-    collection: 'fall_detection_logs'
+    collection: 'sensor_data'
 });
 
 module.exports = mongoose.model('FallDetectionLog', FallDetectionLogSchema);
