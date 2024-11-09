529e0b1c36c59f3dce2dab20c2f5f0fe000ec0cb
ygfest <stefanosanesteban1018@gmail.com>
2024-11-10 02:48:16 +0800

    minor ui change for landing page

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -80,13 +80,13 @@ export default function Home() {
           {/* Card 1 - Real-time Location Tracking */}
           <div
             ref={(el) => (cardsRef.current[0] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[60px]"
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[60px]"
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
                 src="/location.svg"
                 alt="Real-time Location Tracking"
-                className="w-full max-w-[400px] h-auto object-cover rounded-md"
+                className="w-full max-w-[500px] h-auto object-cover rounded-md"
               />
             </div>
             <div className="w-full space-y-4 text-center">
@@ -103,13 +103,13 @@ export default function Home() {
           {/* Card 2 - Intrusion Monitoring */}
           <div
             ref={(el) => (cardsRef.current[1] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px] md:mt-[240px]"
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px] md:mt-[240px]"
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
                 src="/intrusion.svg"
                 alt="Intrusion Monitoring"
-                className="w-full max-w-[400px] h-auto object-cover rounded-md"
+                className="w-full max-w-[500px] h-auto object-cover rounded-md"
               />
             </div>
             <div className="w-full space-y-4 text-center">
@@ -126,13 +126,13 @@ export default function Home() {
           {/* Card 3 - Fall Detection */}
           <div
             ref={(el) => (cardsRef.current[2] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[-180px]"
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[-180px]"
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
                 src="/fall.svg"
                 alt="Fall Detection"
-                className="w-full max-w-[400px] h-auto object-cover rounded-md"
+                className="w-full max-w-[500px] h-auto object-cover rounded-md"
               />
             </div>
             <div className="w-full space-y-4 text-center">
@@ -149,13 +149,13 @@ export default function Home() {
           {/* Card 4 - Temperature Monitoring */}
           <div
             ref={(el) => (cardsRef.current[3] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px] "
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px] "
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
                 src="/temp.svg"
                 alt="Temperature Monitoring"
-                className="w-full max-w-[400px] h-auto object-cover rounded-md"
+                className="w-full max-w-[500px] h-auto object-cover rounded-md"
               />
             </div>
             <div className="w-full space-y-4 text-center">
@@ -172,13 +172,13 @@ export default function Home() {
           {/* Card 5 - Geofencing */}
           <div
             ref={(el) => (cardsRef.current[4] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[-180px]"
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-start transform -translate-y-12 h-[540px] md:mt-[-180px]"
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
                 src="/geofence.svg"
                 alt="Geofencing"
-                className="w-full max-w-[400px] h-auto object-cover rounded-md"
+                className="w-full max-w-[500px] h-auto object-cover rounded-md"
               />
             </div>
             <div className="w-full space-y-4 text-center">
@@ -195,7 +195,7 @@ export default function Home() {
           {/* Card 6 - Reporting Feature */}
           <div
             ref={(el) => (cardsRef.current[5] = el)}
-            className="relative flex flex-col items-center p-8 rounded-lg shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px]"
+            className="relative flex flex-col items-center p-8 rounded-3xl shadow-lg bg-gradient-to-r from-[#F3F4F6] to-[#E5E7EB] self-end transform translate-y-12 h-[540px]"
           >
             <div className="w-full flex justify-center items-center mb-6">
               <img
