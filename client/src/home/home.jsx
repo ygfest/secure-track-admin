75d4b85f45981cf0a8c8bf981335040590d33637
ygfest <stefanosanesteban1018@gmail.com>
2024-11-21 00:24:08 +0800

    Reverted back the changes made

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -50,28 +50,24 @@ export default function Home() {
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#5CC90C,transparent_1px),linear-gradient(to_bottom,#5CC90C,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_70%_60%_at_60%_0%,#000_80%,transparent_110%)]" />
         <nav className="fixed left-1/2 top-4 z-40 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl border-2 border-[#42393962] px-4 py-3 backdrop-blur-sm backdrop-saturate-150 bg-white md:px-8 md:py-4">
           <header className="flex w-full items-center justify-between">
-            {/* Logo */}
             <img src="/st-logo.png" alt="logo" width={120} height={35} />
-
-            {/* Buttons */}
-            <div className="flex flex-wrap gap-2 items-center justify-end">
+            <div className="flex gap-1 md:gap-4 items-center">
               <Link to="/sign-in">
-                <button className="font-medium w-auto px-4 text-sm md:text-base p-2 rounded-lg text-primary hover:bg-zinc-200">
+                <button className="font-medium min-w-[60px] text-sm md:min-w-[100px] md:text-base p-2 rounded-lg text-primary hover:bg-zinc-200 flex-nowrap">
                   Sign in
                 </button>
               </Link>
               <Link to="/sign-up">
-                <button className="hidden md:inline px-4 p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg">
+                <button className="hidden md:inline p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg min-w-[150px] md:min-w-[120px] flex-nowrap">
                   Create an account
                 </button>
-                <button className="md:hidden px-4 p-2 text-sm font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg">
+                <button className="md:hidden p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg min-w-[120px] md:min-w-[120px] flex-nowrap">
                   Create account
                 </button>
               </Link>
             </div>
           </header>
         </nav>
-
         <div className="z-30 w-[90%] max-w-[1200px] pt-4">
           <h1 className="text-center text-[2.3rem] font-bold leading-tight md:text-[3.2rem] md:leading-none lg:text-[4rem] 2xl:text-[4.5rem]">
             <span className="text-black">
