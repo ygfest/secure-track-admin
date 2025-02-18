434932f1bdf02dfe6d87831dfc07bbd92f6cbc71
ygfest <stefanosanesteban1018@gmail.com>
2025-02-18 23:59:56 +0800

    Added a loader for better user experience

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import NavigationBar from "../pages/admin/components/NavigationBar";
 import { Outlet } from "react-router-dom";
 import useAuth from "../hook/useAuth";
+import { BarLoader } from "react-spinners";
 
 const AdminLayout = () => {
   const admin = useAuth("admin");
 
-  if (!admin) return;
+  if (!admin)
+    return (
+      <div className="bg-white h-[100vh] w-[100vw] flex flex-col items-center justify-center">
+        <img src="/ST-without-name.svg" className="h-24 mb-8" />
+        <BarLoader color="#272829" size={40} data-testid="loader" />
+      </div>
+    );
   return (
     <div className="h-screen bg-inherit">
       <NavigationBar />
--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import NavigationBar from "../pages/user/components/NavigationBar";
 import { Outlet } from "react-router-dom";
 import useAuth from "../hook/useAuth";
+import { BarLoader } from "react-spinners";
 
 const UserLayout = () => {
   const user = useAuth("user");
 
-  if (!user) return;
+  if (!user)
+    return (
+      <div className="bg-white h-[100vh] w-[100vw] flex flex-col items-center justify-center">
+        <img src="/ST-without-name.svg" className="h-24 mb-8" />
+        <BarLoader color="#272829" size={40} data-testid="loader" />
+      </div>
+    );
   return (
     <div className="min-h-screen bg-inherit">
       <NavigationBar />
